584c822031281df26e56f1b1f05861a981390376
Rubylenshy <72923646+Rubylenshy@users.noreply.github.com>
2022-04-26 12:00:32 +0100

    Update chessboard.php

--- chessboard.php
+++ chessboard.php
@@ -1,6 +1,31 @@
+<table width = "400px" cellpadding="0px" cellspacing="0px" border="1px" align="center">
+	<div>
+		<h2> Capstone Chessboard </h2>
+	</div>
 <?php
 
+	for ($row=1; $row <= 8 ; $row++) 
 
+	{ 
+		echo "<tr>";
 
+		for ($column=1; $column <= 8 ; $column++) 
+		{ 
+			$total = $row + $column;
+			if ($total%2 == 0) {
+					echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+				}
+
+				else{
+					echo "<td height=30px width=30px bgcolor=#000000></td>";
+				}	
+		
+		}
+
+		echo "</tr>";
+
+	}	
 
 ?>
+
+</table>
